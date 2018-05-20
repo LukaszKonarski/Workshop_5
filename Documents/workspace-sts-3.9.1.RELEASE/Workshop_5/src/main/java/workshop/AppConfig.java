53289ed08e5e9f3a6ada3633d05a1718a872aaa8
Łukasz Konarski <lukasz.a.konarski@gmail.com>
2018-05-20 15:34:00 +0200

    first full commit

---
+++ Documents/workspace-sts-3.9.1.RELEASE/Workshop_5/src/main/java/workshop/AppConfig.java
@@ -0,0 +1,12 @@
+package workshop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "workshop")
+public class AppConfig extends WebMvcConfigurerAdapter {
+}
